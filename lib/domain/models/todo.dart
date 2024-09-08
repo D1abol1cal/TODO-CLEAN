8bc064855f5c6d2feb2d346251ce9552610e552c
Nofel <syednofeltalha2@gmail.com>
2024-09-08 22:48:38 +0500

    Todo model

--- lib/domain/models/todo.dart
+++ lib/domain/models/todo.dart
@@ -4,3 +4,30 @@ TO DO MODEL
 
 this is what a todo object is.
 
+--------------------------------------------------------------------------------
+
+it has these properties:
+
+- id: the unique identifier of the todo
+- text: the title of the todo
+- isCompleted : a boolean value that indicates if the todo is completed or not
+
+--------------------------------------------------------------------------------
+
+Methods:
+
+- toggle completion on & off
+
+*/
+
+class Todo {
+  final int id;
+  final String text;
+  final bool isCompleted;
+
+  Todo({
+    required this.id,
+    required this.text,
+    this.isCompleted = false,
+  });
+}
